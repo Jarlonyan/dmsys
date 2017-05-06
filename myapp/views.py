b299f9432bbb1a9c7de08a3cba0a6dfdfa58de4c
Jarlonyan <286568450@qq.com>
2017-05-06 22:57:17 +0800

    使用ECharts展示图

--- myapp/views.py
+++ myapp/views.py
@@ -11,23 +11,5 @@ def index(request):
 	# model_param['webname']="company公司网站"
 	# return render_to_response('index.html')
 
-	import random
-	import django
-	import datetime
-	import matplotlib.pyplot as plt
 
-	plt.figure(1)
-	x=[]
-	y=[]
-	now=datetime.datetime.now()
-	delta=datetime.timedelta(days=1)
-	for i in range(10):
-		x.append(now)
-		now+=delta
-		y.append(random.randint(0, 1000))
-	plt.plot(x, y, '-')
-	plt.savefig('static/media/test.png')
-	plt.clf()
-	plt.close()
-
-	return render(request, 'index.html', {'image': "/static/media/test.png"})   #
+	return render(request, 'index.html')   #
