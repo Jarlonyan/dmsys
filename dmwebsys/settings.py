d3ad678f0777062ed1103138544af7e8f401f6ea
Jarlonyan <286568450@qq.com>
2017-05-08 16:03:22 +0800

    5.8

--- dmwebsys/settings.py
+++ dmwebsys/settings.py
@@ -44,10 +44,9 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'dmwebsys.urls'
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'dmwebsys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,41 @@
 #coding:utf-8
 
 from django.shortcuts import render,render_to_response
-import django.template #.context import RequestContext
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django import forms
 from myapp import models
 
 # Create your views here.
 '''----------------------------------主页------------------------------------------'''
+
+class IndexUploadFileForm(forms.Form):
+	title=forms.CharField()
+	file=forms.FileField()
+
+def handle_upload_file(f):
+	with open("uploaddata/test.png","wb") as dest:
+		for chunk in f.chunks():
+			dest.write(chunk)
+
+def read_file():
+	return 0
+
 def index(request):
-	# model_param={}
-	# model_param['webname']="company公司网站"
-	# return render_to_response('index.html')
-	return render(request, 'index.html')   #
+	if request.method=="POST":
+		form=IndexUploadFileForm(request.POST,request.FILES)
+		if form.is_valid():
+			print (request.FILES['title'],">>>>>")
+			#print (">>>>>>>>>>> 文件 ", request.FILES['filename'].name , " 上传成功!!")
+			handle_upload_file(request.FILES['file'])
+			return HttpResponseRedirect('/success/url/')
+		else:
+			form=IndexUploadFileForm()
+			return render(request,'index.html',{'uf':form})
+	else:
+		form=IndexUploadFileForm()
+
+	return render(request,'index.html',{'uf':form})   #
 
 
 def classification(request):
